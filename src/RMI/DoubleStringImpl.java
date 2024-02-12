2cbe7d6dabb89626fd31a8770b13195467850c65
v sotnikov <sotnikov@rosplatforma.ru>
2024-02-12 11:24:27 +0300

    12022024commit

---
+++ src/RMI/DoubleStringImpl.java
@@ -0,0 +1,9 @@
+package RMI;
+
+import java.rmi.RemoteException;
+
+public class DoubleStringImpl implements DoubleString {
+    public String doubleString(String str) throws RemoteException {
+        return str + str;
+    }
+}
